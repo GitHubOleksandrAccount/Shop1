20c830b768aeaf85d827e505d67047089b199619
GitHubOleksandrAccount <161873463+GitHubOleksandrAccount@users.noreply.github.com>
2024-03-08 19:06:31 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,8 @@
+# Usage with NPM
+$ npm install --save @fancyapps/ui
+
+# and with Yarn
+$ yarn add @fancyapps/ui
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+Stackblitz Example
